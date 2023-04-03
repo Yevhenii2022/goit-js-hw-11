d6f1ea56bd52737756b9d54f63d90989cb2bcf44
Yevhenii2022 <3starosta3@gmail.com>
2023-04-03 12:19:46 +0300

    added load-more without style

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,29 +1,26 @@
 import axios from 'axios';
 
-export async function getImages(query) {
-  const API_KEY = '34887030-f3c1ca9c5b1c016c795af66f2';
-  const BASE_URL = 'https://pixabay.com/api/';
+export class ImagesAPI {
+  #API_KEY = '34887030-f3c1ca9c5b1c016c795af66f2';
+  #BASE_URL = 'https://pixabay.com/api/';
+  page = 1;
+  query = null;
+  per_page = 40;
 
-  try {
-    const response = await axios.get(`${BASE_URL}`, {
+  async getImages() {
+    const response = await axios.get(`${this.#BASE_URL}`, {
       params: {
-        key: API_KEY,
-        q: query,
+        key: this.#API_KEY,
+        q: this.query,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
-        page: 1,
-        per_page: 40,
+        page: this.page,
+        per_page: this.per_page,
       },
     });
 
-    // if (!response.ok) {
-    //   throw new Error(response.status);
-    // }
-
-    console.log(response.data.hits);
-    // return response;
-  } catch (error) {
-    throw error;
+    console.log(response.data);
+    return response.data;
   }
 }
--- src/js/load-more.js
+++ src/js/load-more.js
@@ -1,32 +1,129 @@
 import Notiflix from 'notiflix';
-import { getImages } from './fetch-images';
+import { ImagesAPI } from './fetch-images';
 
-getImages('sex');
-
+const formEl = document.getElementById('search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
+
+const imagesAPI = new ImagesAPI();
+
+isLoadMoreBtnShow(false);
+
+formEl.addEventListener('submit', handleFormElSubmit);
+loadMoreBtnEl.addEventListener('click', handleLoadMoreBtnElClick);
+
+async function handleFormElSubmit(event) {
+  event.preventDefault();
+
+  isLoadMoreBtnShow(false);
+
+  galleryEl.innerHTML = '';
+
+  imagesAPI.page = 1;
+
+  imagesAPI.query = event.currentTarget.elements.searchQuery.value.trim();
+
+  if (imagesAPI.query === '') {
+    console.log('nothing');
+    return;
+  }
+
+  try {
+    const { hits: images, totalHits: totalQuantity } =
+      await imagesAPI.getImages();
+    console.log(images);
+
+    if (!totalQuantity) {
+      console.log('not found');
+      return;
+    }
+
+    renderGalleryList(images);
+
+    if (totalQuantity < imagesAPI.page * imagesAPI.per_page) {
+      isLoadMoreBtnShow(false);
+      console.log('endddd');
+      return;
+    }
+
+    isLoadMoreBtnShow(true);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handleLoadMoreBtnElClick(event) {
+  imagesAPI.page += 1;
+
+  try {
+    const { hits: images, totalHits: totalQuantity } =
+      await imagesAPI.getImages();
+    console.log(images);
+
+    renderGalleryList(images);
+
+    autoScrollPage();
+
+    if (totalQuantity < imagesAPI.page * imagesAPI.per_page) {
+      isLoadMoreBtnShow(false);
+      console.log('endddd');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function renderGalleryList(gallerys) {
   const markup = gallerys
     .map(
-      gallery => `<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<a href="${largeImageURL}">
+        <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width=300px />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
+      ${likes}
     </p>
     <p class="info-item">
       <b>Views</b>
+      ${views}
     </p>
     <p class="info-item">
       <b>Comments</b>
+      ${comments}
     </p>
     <p class="info-item">
       <b>Downloads</b>
+      ${downloads}
     </p>
   </div>
-</div>`
+</div>
+</a>`
     )
     .join('');
-  console.log(webformatURL);
-  galleryEl.innerHTML = markup;
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function isLoadMoreBtnShow(boolean) {
+  loadMoreBtnEl.style.display = boolean ? 'block' : 'none';
+}
+
+function autoScrollPage() {
+  const { height: cardHeight } =
+    galleryEl.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
